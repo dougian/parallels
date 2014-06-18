bd48a6098c23b568a80e8316a53545c03539d919
dougian <dougian@outlook.com>
2014-06-18 16:02:11 +0300

    A carry lookahead is build ground-up correctly

--- carry_look.py
+++ carry_look.py
@@ -13,7 +13,11 @@ class Node(object):
         self.right_son = r_son
 
     def __str__(self):
-        return "Value = %d", self.value
+        return "Node on level %d, value %d " % (self.level, self.value)
+
+
+    def __repr__(self):
+        return "Node on level %d, value %d " % (self.level, self.value)
 
 class lookupTree(object):
 
@@ -25,36 +29,39 @@ class lookupTree(object):
 
         nodes = [Node(val, 0) for val in valuesvec]
 
-        num_pairs = ceil(len(valuesvec) / 2)
+        num_pairs = len(valuesvec) / 2
         it = 0
 
         while(num_pairs > 0):
 
             it += 1
-            temp = [Node(0, it, nodes[i], nodes[i+1]) for i in range(int(num_pairs))]
+            temp = [Node(0, it, nodes[2*i], nodes[2*i+1]) for i in range(int(num_pairs))]
 
             if len(temp) == 1:
                 self.root = temp[0]
 
-            for j in temp:
-                print(j.value, j.level)
+            print(temp)
+
             nodes = temp
             num_pairs = len(nodes) / 2
+
+        return self.root
 
     def printTree(self, root):
         if root == None:
             pass
         else:
             self.printTree(root.left_son)
-            print(root.value)
+            print(root)
             self.printTree(root.right_son)
+
+            
 
 
 a  = [1, 2, 3, 4]
 mytree = lookupTree()
-mytree.build_tree(a)
-print(mytree.root.value, mytree.root.level)
-mytree.printTree(mytree.root)
+r = mytree.build_tree(a)
+mytree.printTree(r)
 
 
 __author__ = 'dougian'
